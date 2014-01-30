076009855183bac9a5c01aa998d38b6c6fa2a49f
Louis Muhammad <louis.muhammad@gmail.com>
2014-01-30 04:40:27 -0500

    About Triangle Project 2

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,13 +14,19 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
- if a == b && a == c
-	 :equilateral
- elsif a == b || a == c || b == c
-	 :isosceles
- else
-	 :scalene
- end
+
+  if a <= 0 ||  b <= 0 || c <= 0
+    raise TriangleError
+  end 
+
+  if a + b <= c  || a + c <= b ||  b + c <= a
+    raise TriangleError
+  end 
+
+  return :equilateral if a == b && b == c
+  return :isosceles   if a == b || a == c ||  b == c
+  return :scalene     if a != b && a != c &&  b != c 
+
 end
 
 # Error class used in part 2.  No need to change this code.
