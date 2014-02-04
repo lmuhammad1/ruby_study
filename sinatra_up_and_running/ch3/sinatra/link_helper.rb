d6b3d3630ebad9abc3e65d2b1becf684c24d1191
Louis Muhammad <louis.muhammad@gmail.com>
2014-02-04 06:37:03 -0500

    Finished up Ch 3

---
+++ sinatra_up_and_running/ch3/sinatra/link_helper.rb
@@ -0,0 +1,12 @@
+module Sinatra
+	module LinkHelper
+		def link(name)
+			case name
+			when :about then '/about'
+			when :index then '/index'
+			else "/page/#{name}"
+			end
+		end
+	end
+	helpers LinkHelper
+end
