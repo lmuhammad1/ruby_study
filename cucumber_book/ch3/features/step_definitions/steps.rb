0bcf3ddcf2a8f80f5ffc428a1041b1a7ac37bfcb
Louis Muhammad <louis.muhammad@gmail.com>
2014-02-10 21:31:33 -0500

    Finished with Chapter 4

---
+++ cucumber_book/ch3/features/step_definitions/steps.rb
@@ -0,0 +1,10 @@
+Given /^I have deposited \$(\d+) in my account$/ do |amount| 
+	Account.new(amount.to_i)
+end
+
+When /^I request \$(\d+)$/ do |amount|
+pending("How do we simulate cash being requested?") end
+
+Then /^\$(\d+) should be dispensed$/ do |amount| 
+	pending("How do we validate that cash was dispensed?")
+end
