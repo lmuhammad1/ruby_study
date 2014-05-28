3f1471ffc9c47c30cce7e8398b73deec2706ad6d
Louis Muhammad <louis.muhammad@gmail.com>
2014-05-27 20:48:56 -0400

    222 Problem

--- codewars/kyu5/largest_5_digit_number_in_a_series.rb
+++ codewars/kyu5/largest_5_digit_number_in_a_series.rb
@@ -1,27 +1,44 @@
-def solution(digits)
-	counter = 0
-	temp_char = '0'
-	temp_number = ''
-	final_number = ''
+#  Description:
 
-	digits.each_char do |num|
-		
-		if num >= temp_char
-			temp_char = num
-			temp_number = digits.slice(counter, 5)
-			if temp_number > final_number
-				final_number = temp_number
-			end
-		end
-		
-		counter = counter + 1
-	end
-	return final_number.to_i
-end
+#  In the following 6 digit number:
 
-# best kata solution
+#  283910
+#  91 is the greatest sequence of 2 digits.
+
+#  Complete the solution so that it returns the largest five digit number 
+#  found within within the number given.. The number will be passed in as 
+#  a string of only digits. It should return a five digit integer. 
+#  The number passed may be as large as 1000 digits.
+
 # def solution(digits)
-#   digits.split('').each_cons(5).max.join.to_i
+# 	counter = 0
+# 	temp_char = '0'
+# 	temp_number = ''
+# 	final_number = ''
+
+# 	digits.each_char do |num|
+		
+# 		if num >= temp_char
+# 			temp_char = num
+# 			temp_number = digits.slice(counter, 5)
+# 			if temp_number > final_number
+# 				final_number = temp_number
+# 			end
+# 		end
+		
+# 		counter = counter + 1
+# 	end
+# 	return final_number.to_i
 # end
 
-p solution ('283910073479988')
+# p solution ('283910073479988')
+
+
+best kata solution
+def solution(digits)
+  digits.split('').each_cons(5).max.join.to_i
+end
+
+p solution ('2839100734799886354255')
+
+
