6e84404d3d949a221e187f44924e1b312c4149eb
Louis Muhammad <louis.muhammad@gmail.com>
2015-01-04 13:00:29 -0500

    Kyu 7 -- Remove duplicates (added new solution)

--- codewars/kyu7/remove_duplicates.rb
+++ codewars/kyu7/remove_duplicates.rb
@@ -8,6 +8,14 @@ def unique(integers)
   array
 end
 
+def unique2(integers)
+  integers & integers
+end
+
+
 p unique([5,2,1,3])
 p unique([5,2,1,3,3,3,2])
+
+p unique2([5,2,1,3])
+p unique2([5,2,1,3,3,3,2])
 
