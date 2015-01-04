8388c226e64ef9052d228926b4a4b6fdf5bd3257
Louis Muhammad <louis.muhammad@gmail.com>
2015-01-04 11:01:25 -0500

    Added grade F to Grader

--- codewars/kyu8/grader.rb
+++ codewars/kyu8/grader.rb
@@ -4,9 +4,11 @@ def grader(score)
   when (0.8)..(0.89) then "B"
   when (0.7)..(0.79) then "C"
   when (0.6)..(0.69) then "D"
+  else "F"
   end
 end
 
 p grader(0.7)
 p grader(0.9)
 p grader(0.6)
+p grader(1.01)
