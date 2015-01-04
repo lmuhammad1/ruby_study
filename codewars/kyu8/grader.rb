5e10db58c8310011c4c873b7dad1080caa021c57
Louis Muhammad <louis.muhammad@gmail.com>
2015-01-04 10:43:40 -0500

    Kyu 8 -- Grader

---
+++ codewars/kyu8/grader.rb
@@ -0,0 +1,12 @@
+def grader(score)
+  case score
+  when (0.9)..(1.00) then "A"
+  when (0.8)..(0.89) then "B"
+  when (0.7)..(0.79) then "C"
+  when (0.6)..(0.69) then "D"
+  end
+end
+
+p grader(0.7)
+p grader(0.9)
+p grader(0.6)
